fc72f82c28b236023593c36523601f32f50f78f4
Cainã Brazil <caina.brazil@jumia.com>
2018-08-28 11:56:06 +0100

    using endsWith

--- public/js/sw/index.js
+++ public/js/sw/index.js
@@ -1,9 +1,9 @@
 self.addEventListener('fetch', function (event) {
   // TODO: only respond to requests with a
   // url ending in ".jpg"
-  let fileExtension = event.request.url.substr(event.request.url.length - 4)
-  if (fileExtension === '.jpg')
+  if (event.request.url.endsWith('.jpg')) {
     event.respondWith(
       fetch('/imgs/dr-evil.gif')
     );
+  }
 });
