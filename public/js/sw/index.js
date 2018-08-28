c4b27b23758e01cb1d251cccbbcb6dbc30dce6fe
Cainã Brazil <caina.brazil@jumia.com>
2018-08-28 11:54:55 +0100

    intercepting jpgs only

--- public/js/sw/index.js
+++ public/js/sw/index.js
@@ -1,7 +1,9 @@
-self.addEventListener('fetch', function(event) {
+self.addEventListener('fetch', function (event) {
   // TODO: only respond to requests with a
   // url ending in ".jpg"
-  event.respondWith(
-    fetch('/imgs/dr-evil.gif')
-  );
+  let fileExtension = event.request.url.substr(event.request.url.length - 4)
+  if (fileExtension === '.jpg')
+    event.respondWith(
+      fetch('/imgs/dr-evil.gif')
+    );
 });
